c32fb2e23a992297e79ee30337a47444afe9ca20
Raggan2000 <ernstn@student.chalmers.se>
2022-02-01 14:25:12 +0100

    Vill också hälsa

--- src/Othello/Main.java
+++ src/Othello/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	justALittleClass lilClass = new justALittleClass(1);
-    lilClass.printSomething();
-    System.out.println("hej hej mvh Lovisa");
+        lilClass.printSomething();
+        System.out.println("hej hej mvh Lovisa");
+        System.out.println("hej hej mvh Ernst");
 
     }
 }
