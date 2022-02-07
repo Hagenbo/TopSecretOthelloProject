10e055667937081863e9f6325bea06025909383c
Vhagenbo <77843990+Vhagenbo@users.noreply.github.com>
2022-02-07 11:40:57 -0500

    Created Player-class. Just a start...

---
+++ src/Othello/model/Player.java
@@ -0,0 +1,27 @@
+package Othello.model;
+
+// So far: a new player is created with each new game and their information is stored when a game is saved
+// do we want an account to be set up that can be reused for several games?
+public class Player {
+
+    private final String userName;
+    //private String password;
+    private PieceColor color;
+
+    public Player(String uname) {
+        this.userName = uname;
+        //this.password = password;
+    }
+
+    public void assignColor(PieceColor c) {  //called when a game starts
+        this.color = c;
+    }
+
+    public PieceColor getColor() {
+        return this.color;
+    }
+
+    public String getPlayerName() {
+        return this.userName;
+    }
+}
