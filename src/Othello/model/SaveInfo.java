39825479044398dd8d1dabbbe64d5d05fcf1026f
lovix182 <rosin.lovisa@gmail.com>
2022-02-25 15:17:24 +0100

    SaveInfo class

---
+++ src/Othello/model/SaveInfo.java
@@ -0,0 +1,44 @@
+package Othello.model;
+
+import Othello.othelloController.Options;
+
+import java.io.Serializable;
+
+public class SaveInfo implements Serializable {
+    private Board b;
+    private Player p1;
+    private Player p2;
+    private PieceColor playerTurn;
+    private Options o;
+
+    public SaveInfo(Board b, Player p1, Player p2, PieceColor playerTurn, Options o) {
+        this.b = b;
+        this.p1 = p1;
+        this.p2 = p2;
+        this.playerTurn = playerTurn;
+        this.o = o;
+    }
+
+    public String getP1(){
+        return p1.getPlayerName();
+    }
+
+    public String getP2(){
+        return p2.getPlayerName();
+    }
+
+    public Board getBoard(){
+        return b;
+    }
+
+    public PieceColor getPlayerTurn(){
+        return playerTurn;
+    }
+
+    public Options getOptions(){
+        return o;
+    }
+
+
+
+}
